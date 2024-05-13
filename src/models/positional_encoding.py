6bb06b0238c3c7aaddc21ae922ef6cb501b9f6c2
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2024-05-13 15:27:14 +0300

    v1

--- src/models/positional_encoding.py
+++ src/models/positional_encoding.py
@@ -1,19 +1,25 @@
 import torch
+import numpy as np
+
 
 class PositionalEncoding(torch.nn.Module):
-    def __init__(self, emb_size, maxlen):
+    def __init__(self, emb_size, max_len):
         """
-        emb_size - размер эмбеддингов
-        maxlen - длинна контекста
+        emb_size - размер эмбеддингов (E - encoder_embedding)
+        max_sent_len - длинна контекста (S - sequence)
         """
         super(PositionalEncoding, self).__init__()
-        # TODO: Реализуйте конструтор
-        pass
 
+        pos_encoding = torch.zeros(max_len, emb_size) 
+        positions = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        denominator = torch.exp(-np.log(10000) * torch.arange(0, emb_size, 2).float() / emb_size)
+        pos_encoding[:, 0::2] = torch.sin(positions * denominator)
+        pos_encoding[:, 1::2] = torch.cos(positions * denominator)
+        pos_encoding = pos_encoding.unsqueeze(1)
+        self.register_buffer("pos_encoding", pos_encoding)
 
-    def forward(self, token_embedding):
+    def forward(self, token_embedding: torch.Tensor):
         """
-        token_embedding - тензор матрицы эмбеддингов
+        token_embedding - тензор матрицы эмбеддингов: (S, B, E)
         """
-        # TODO: Реализуйте сложение эмбединнгов токенов с позиционными эмбеддингами
-        pass
+        return token_embedding + self.pos_encoding[:token_embedding.size(0)]
